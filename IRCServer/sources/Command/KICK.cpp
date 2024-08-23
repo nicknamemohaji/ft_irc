601d662cc28f5e229f26e5c404be34b229830d47
somilee <somilee@student.42seoul.kr>
2024-08-23 22:01:27 +0900

    mode fix

---
+++ IRCServer/sources/Command/KICK.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <deque>
+#include <algorithm>
+#include <queue>
+
+#include "IRCServer.hpp"
+#include "IRCChannel.hpp"
+#include "IRCClient.hpp"
+#include "IRCContext.hpp"
+#include "IRCErrors.hpp"
+
+// void IRCServer::ActionKICK(IRCContext& context)
+// {
+
+// }
